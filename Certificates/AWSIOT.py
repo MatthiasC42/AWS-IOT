4e0a2e026526c2e769ddefc1fecb6977896629cf
MatthiasC42 <115440405+MatthiasC42@users.noreply.github.com>
2022-10-10 11:38:46 +0800

    Create AWSIOT.py

---
+++ Certificates/AWSIOT.py
@@ -0,0 +1,17 @@
+import time
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
+
+GPIO.setmode(GPIO.BOARD)
+myMQTTClient = AWSIoTMQTTClient("RishabClientID") #random key, if another connection using the same key is opened the previous one is auto closed by AWS IOT
+myMQTTClient.configureEndpoint("apdgyoei7c8e7-ats.iot.us-east-1.amazonaws.com", 8883)
+
+myMQTTClient.configureCredentials("/AWS-IoT/Certificates/AmazonRootCA1.pem",
+ "/AWS-IoT/Certificates/60b1321bad028ee4d96d2706b6a6783eccf6e45a3583cb50a63ab579a2042611-private.pem.key",
+ "/AWS-IoT/Certificates/60b1321bad028ee4d96d2706b6a6783eccf6e45a3583cb50a63ab579a2042611-certificate.pem.crt")
+
+myMQTTClient.configureOfflinePublishQueueing(-1) # Infinite offline Publish queueing
+myMQTTClient.configureDrainingFrequency(2) # Draining: 2 Hz
+myMQTTClient.configureConnectDisconnectTimeout(10) # 10 sec
+myMQTTClient.configureMQTTOperationTimeout(5) # 5 sec
+print ('Initiating Realtime Data Transfer From Raspberry Pi...')
+myMQTTClient.connect()
