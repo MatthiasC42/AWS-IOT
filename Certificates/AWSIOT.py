e36cb4fc52e2afced3a69afead553b18590df798
MatthiasC42 <115440405+MatthiasC42@users.noreply.github.com>
2022-11-23 13:28:33 +0800

    Update AWSIOT.py

--- Certificates/AWSIOT.py
+++ Certificates/AWSIOT.py
@@ -25,7 +25,23 @@ myMQTTClient.connect()
 
 print("Publishing Message from RPI")
 myMQTTClient.publish(
- topic="home/helloworld",
- QoS=1,
- payload="{'Message':'Message By RPI'}"
+file1 = open('28_jpg.txt', 'r')
+count = 0
+  
+while True:
+    count += 1
+  
+    # Get next line from file
+    line = file1.readline()
+  
+    # if line is empty
+    # end of file is reached
+    if not line:
+        break
+    topic="home/helloworld",
+    QoS=1,
+    payload="{'Message':'Message By RPI'}"
+    print("Line{}: {}".format(count, line.strip()))
+  
+file1.close()
 )
