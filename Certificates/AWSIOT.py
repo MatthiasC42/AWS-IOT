f41b0ed76573d3dbdf711dafc515915d408f6863
MatthiasC42 <115440405+MatthiasC42@users.noreply.github.com>
2022-11-28 15:31:24 +0800

    Update AWSIOT.py

--- Certificates/AWSIOT.py
+++ Certificates/AWSIOT.py
@@ -8,7 +8,7 @@ from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
 def read_text_file(file_path):
     with open(file_path, 'r') as line:  
         lines = line.readlines()
-        line_split = lines.rstrip("\n").split(' ')
+        line_split = lines.split(' ')
         data = json.dumps(getData(line_split))
         print(line_split)
         myMQTTClient.publish(
